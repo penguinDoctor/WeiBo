144c46a12dcc325e8167887e1a359c322da33feb
15221532825 <2210515471@qq.com>
2020-06-05 18:34:54 +0800

    Initial Commit

---
+++ FXWeiBo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FXWeiBo
+//
+//  Created by 飞翔 on 2020/6/5.
+//  Copyright © 2020 飞翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FXWeiBoTests/FXWeiBoTests.swift
@@ -0,0 +1,34 @@
+//
+//  FXWeiBoTests.swift
+//  FXWeiBoTests
+//
+//  Created by 飞翔 on 2020/6/5.
+//  Copyright © 2020 飞翔. All rights reserved.
+//
+
+import XCTest
+@testable import FXWeiBo
+
+class FXWeiBoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
